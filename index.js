b82ddcbed911a231631ae96c9b8cbc0845e9d868
Abraham <abukip344@gmail.com>
2024-03-23 04:55:26 -0400

    countdown

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// countdown to zero
+function countDown(myNumber) {
+    for (let i = myNumber; i >= 0; i--){
+        console.log(i);
+    }
+}
+
+//thank you card
+function writeCards(nameArray, theEvent) {
+    let messageArray = [];
+    for (let i = 0; i < nameArray.length; i++){
+        messageArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${theEvent} gift!`);
+    }
+    return messageArray;
+}
+
+    
+
+
+
